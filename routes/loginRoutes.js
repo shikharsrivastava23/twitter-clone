c66c0c955dc76bcc38d3b978d3ff3e50d3478edb
Shikhar <f20170794@hyderabad.bits-pilani.ac.in>
2021-02-20 20:16:00 +0530

    basic styling for main page

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
 const bcrypt = require("bcrypt");
-const User = require("../schemas/UserSchema");
+const User = require('../schemas/UserSchema');
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -11,31 +11,31 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
+    
     res.status(200).render("login");
-});
+})
 
 router.post("/", async (req, res, next) => {
+
     var payload = req.body;
 
-    if (req.body.logUsername && req.body.logPassword) {
+    if(req.body.logUsername && req.body.logPassword) {
         var user = await User.findOne({
             $or: [
                 { username: req.body.logUsername },
-                { email: req.body.logUsername },
-            ],
-        }).catch((error) => {
+                { email: req.body.logUsername }
+            ]
+        })
+        .catch((error) => {
             console.log(error);
             payload.errorMessage = "Something went wrong.";
             res.status(200).render("login", payload);
         });
+        
+        if(user != null) {
+            var result = await bcrypt.compare(req.body.logPassword, user.password);
 
-        if (user != null) {
-            var result = await bcrypt.compare(
-                req.body.logPassword,
-                user.password
-            );
-
-            if (result === true) {
+            if(result === true) {
                 req.session.user = user;
                 return res.redirect("/");
             }
@@ -47,6 +47,6 @@ router.post("/", async (req, res, next) => {
 
     payload.errorMessage = "Make sure each field has a valid value.";
     res.status(200).render("login");
-});
+})
 
 module.exports = router;
