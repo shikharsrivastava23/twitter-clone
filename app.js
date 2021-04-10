c0f7b5de60077c6a417015ebe86c4ea02dceb53f
shikharsrivastava23 <shikharsrivastava234@gmail.com>
2021-04-10 21:25:22 +0530

    display user post

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const session = require("express-session");
 const middleware = require("./middleware");
 const path = require("path");
 const app = express();
-const port = 3003;
+const port = 3009;
 
 const mongoose = require("./database");
 
 const server = app.listen(port, () =>
-    console.log("Server listening on port 3003")
+    console.log(`Server listening on port ${port}`)
 );
 
 app.set("view engine", "pug");
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use(
-    session({
+    session({ 
         secret: "jalapenos",
         resave: true,
         saveUninitialized: false,
@@ -28,8 +28,12 @@ app.use(
 
 const loginRoute = require("./routes/loginRoutes");
 app.use("/login", loginRoute);
+
 const registerRoute = require("./routes/registerRoutes");
 app.use("/register", registerRoute);
+
+const postsApiRoute = require("./routes/api/posts");
+app.use("/api/posts", postsApiRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
     let payload = {
