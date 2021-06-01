ff0f7fb444cc92d20866aea4993097e195cf3f12
shikharsrivastava23 <shikharsrivastava234@gmail.com>
2021-06-01 17:41:53 +0530

    User profile page added with styling

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -20,7 +20,14 @@ router.get("/", (req, res, next) => {
 router.get("/:username", async (req, res, next) => {
 
     var payload = await getPayload(req.params.username, req.session.user);
-    console.log(payload)
+    
+    res.status(200).render("profilePage", payload);
+})
+
+router.get("/:username/replies", async (req, res, next) => {
+
+    var payload = await getPayload(req.params.username, req.session.user);
+    payload.selectedTab = "replies";
     
     res.status(200).render("profilePage", payload);
 })
@@ -29,10 +36,15 @@ async function getPayload(username, userLoggedIn) {
     var user = await User.findOne({ username: username })
 
     if(user == null) {
-        return {
-            pageTitle: "User not found",
-            userLoggedIn: userLoggedIn,
-            userLoggedInJs: JSON.stringify(userLoggedIn)
+
+        user = await User.findById(username);
+
+        if (user == null) {
+            return {
+                pageTitle: "User not found",
+                userLoggedIn: userLoggedIn,
+                userLoggedInJs: JSON.stringify(userLoggedIn)
+            }
         }
     }
 
