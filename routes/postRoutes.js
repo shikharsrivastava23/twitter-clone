b8bb4ab6b7c4d8c40a79610e31aab9b217072b76
shikharsrivastava23 <shikharsrivastava234@gmail.com>
2021-06-09 00:32:48 +0530

    Following and followers feature added

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,19 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
 const bcrypt = require("bcrypt");
-const User = require("../schemas/UserSchema");
+const User = require('../schemas/UserSchema');
 
 router.get("/:id", (req, res, next) => {
+
     var payload = {
         pageTitle: "View post",
         userLoggedIn: req.session.user,
         userLoggedInJs: JSON.stringify(req.session.user),
-        postId: req.params.id,
-    };
-
+        postId: req.params.id
+    }
+    
     res.status(200).render("postPage", payload);
-});
+})
 
 module.exports = router;
