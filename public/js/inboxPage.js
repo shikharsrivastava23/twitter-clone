8642406c23e7cbbc1dd031448d223579f4b13ea3
Shikhar <shikhar@mapmyindia.com>
2021-07-18 23:26:54 +0530

    completing chat feature

--- public/js/inboxPage.js
+++ public/js/inboxPage.js
@@ -22,7 +22,7 @@ function outputChatList(chatList, container) {
 function createChatHtml(chatData) {
     var chatName = getChatName(chatData);
     var image = getChatImageElements(chatData);
-    var latestMessage = "This is the latest message";
+    var latestMessage = getLatestMessage(chatData.latestMessage);
 
     return `<a href='/messages/${chatData._id}' class='resultListItem'>
                 ${image}
@@ -33,24 +33,13 @@ function createChatHtml(chatData) {
             </a>`;
 }
 
-function getChatName(chatData) {
-    var chatName = chatData.chatName;
-
-    if (!chatName) {
-        var otherChatUsers = getOtherChatUsers(chatData.users);
-        var namesArray = otherChatUsers.map(
-            user => user.firstName + " " + user.lastName
-        );
-        chatName = namesArray.join(", ");
+function getLatestMessage(latestMessage) {
+    if (latestMessage != null) {
+        var sender = latestMessage.sender;
+        return `${sender.firstName} ${sender.lastName}: ${latestMessage.content}`;
     }
 
-    return chatName;
-}
-
-function getOtherChatUsers(users) {
-    if (users.length == 1) return users;
-
-    return users.filter(user => user._id != userLoggedIn._id);
+    return "New chat";
 }
 
 function getChatImageElements(chatData) {
--- routes/api/chats.js
+++ routes/api/chats.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const User = require("../../schemas/UserSchema");
 const Post = require("../../schemas/PostSchema");
 const Chat = require("../../schemas/ChatSchema");
+const Message = require("../../schemas/MessageSchema");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -39,8 +40,14 @@ router.post("/", async (req, res, next) => {
 router.get("/", async (req, res, next) => {
     Chat.find({ users: { $elemMatch: { $eq: req.session.user._id } } })
         .populate("users")
+        .populate("latestMessage")
         .sort({ updatedAt: -1 })
-        .then(results => res.status(200).send(results))
+        .then(async results => {
+            results = await User.populate(results, {
+                path: "latestMessage.sender",
+            });
+            res.status(200).send(results);
+        })
         .catch(error => {
             console.log(error);
             res.sendStatus(400);
@@ -69,4 +76,13 @@ router.put("/:chatId", async (req, res, next) => {
         });
 });
 
+router.get("/:chatId/messages", async (req, res, next) => {
+    Message.find({ chat: req.params.chatId })
+        .populate("sender")
+        .then(results => res.status(200).send(results))
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(400);
+        });
+});
 module.exports = router;
